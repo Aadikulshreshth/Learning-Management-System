08d970d04ba069f3cbde4e8f2cc4cd73aaeac560
Aadi Kulshreshth <kulshreshthaadi@gmail.com>
2026-03-30 23:01:23 +0530

    Create User.java

---
+++ Models/User.java
@@ -0,0 +1,27 @@
+public abstract class User{
+  protected String UserID;
+  protected String name;
+  protected String email;
+
+  public User(String UserID, String name, String emal){
+    this.UserID=UserID;
+    this.name=name;
+    this.email=email;
+  }
+  //Getters
+  public String GetUserID(){
+    return UserID;
+  }
+
+  public String Getname(){
+    return name;
+  }
+
+  public String Getemail(){
+    return email;
+  }
+
+  //abstract method
+  public abstract void viewDashboard();
+}
+  
