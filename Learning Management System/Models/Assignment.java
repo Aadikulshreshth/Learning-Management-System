e33501a00df8f928cca1939d7f49e762360dd0a2
AADI <kulshreshthaadi@gmail.com>
2026-04-03 13:24:42 +0530

    Added Assignment.java

---
+++ Learning Management System/Models/Assignment.java
@@ -0,0 +1,20 @@
+public class Assignment {
+    private String assignmentID;
+    private String title;
+    private String description;
+    private String deadline;
+    private int maxMarks;
+
+
+    public  Assignment(String assignmentID, String title, String description, String deadline, int maxMarks){
+        this.assignmentID=assignmentID;
+        this.title=title;
+        this.description=description;
+        this.deadline=deadline;
+        this.maxMarks=maxMarks;
+    }
+
+    public String getAssignmentID() {
+        return assignmentID;
+    }
+}
