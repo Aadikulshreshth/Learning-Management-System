6725866636d7230559facc8121c7e03e030729d9
Ashwin <ashwinv365@gmail.com>
2026-04-28 12:28:06 +0530

    Added assignment and exam grading implementations

--- grading/AssignmentGrading.java
+++ grading/AssignmentGrading.java
@@ -1,9 +1,10 @@
 package lms.grading;
+import lms.Models.*;
 
 public class AssignmentGrading implements Evaluator {
 
     @Override
-    public double evaluate(Submission submission, Attendance attendance) {
+    public double evaluate(lms.Models.Submission submission, lms.Models.Attendance attendance) {
         return submission.getMarks() * 0.8;
     }
 }
